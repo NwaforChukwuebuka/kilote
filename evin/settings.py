9f7b46883a7c167d3d9a74a2948a595c312432e9
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-10-17 12:34:38 +0100

    Removed sqlite as the db

--- evin/settings.py
+++ evin/settings.py
@@ -132,12 +132,12 @@ WSGI_APPLICATION = 'evin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
