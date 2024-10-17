9d3a3161e37c38dc90b147933668df5ec5909bd6
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-10-17 14:15:24 +0100

    Added old settings back

--- evin/settings.py
+++ evin/settings.py
@@ -50,24 +50,14 @@ AUTHENTICATION_BACKENDS = (
    
 )
 # Application definition
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DATABASE_NAME'),
-#         'USER': config('DATABASE_USER'),
-#         'PASSWORD': config('DATABASE_PASSWORD'),
-#         'HOST': config('DATABASE_HOST'),
-#         'PORT': config('DATABASE_PORT', default=''),  # Default to empty string if not set
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DATABASE'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRES_HOST'),
-        'PORT': config('POSTGRES_PORT', default='5432'),  # Default to empty string if not set
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT', default=''),  # Default to empty string if not set
     }
 }
 
